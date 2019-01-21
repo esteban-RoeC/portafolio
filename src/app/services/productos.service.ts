4073c5605ba42a90b86caf996ae14fb69cae37d1
erojas <erojas@todo1.net>
2019-01-21 15:09:58 -0500

    Seccion /

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { timeout } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
 
@@ -18,14 +18,55 @@ export class ProductosService {
   }
   private cargarProductos() {
 
-    this.http.get('https://eresolution.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
-      console.log(resp);
+    return new Promise((resolve, reject) => {
 
-      this.productos = resp;
-
-      setTimeout(() => {
+      this.http.get('https://eresolution.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
+        console.log(resp);
+        this.productos = resp;
         this.cargando = false;
-      }, 2000);
+        resolve();
+      });
+
+    });
+  }
+
+  getProducto(id: string) {
+
+    return this.http.get(`https://eresolution.firebaseio.com/productos/${id}.json`);
+
+  }
+
+  buscarProducto(termino: string) {
+
+    if (this.productos.length === 0) {
+      // cargar productos
+
+      this.cargarProductos().then(() => {
+        // Ejecuta después de tener productos
+        // Aplicar filtro
+        this.filtarProductos(termino);
+      });
+
+    } else {
+      // aplicar filtro
+      this.filtarProductos(termino);
+    }
+  }
+
+  private filtarProductos(termino: string) {
+
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
 
     });
 
